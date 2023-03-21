07d91d72f3ec88f6313ea3be7a6358059e0638b7
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-03-21 17:52:02 +0100

    add .biglietto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+let name = document.getElementById('name').value;
 let km = document.getElementById('km-da-percorrere').value;
 let age = document.getElementById('eta').value;
 const genera = document.querySelector('button');
@@ -13,14 +14,21 @@ genera.addEventListener('click', function() {
 
   if (age === 'under18'){
     totale -= (standard / 100) * 20;
+    document.getElementById('offerta').innerHTML = 'Biglietto Under18';
     console.log('totale',totale);
   }else if (age === 'over65'){
     totale -= (standard / 100) * 40;
+    document.getElementById('offerta').innerHTML = 'Biglietto Over65';
     console.log('totale',totale);
   }else{
+    document.getElementById('offerta').innerHTML = 'Biglietto Standard';
     console.log('totale',totale);
   }
 
+  document.getElementById('passeggero').innerHTML = name;
+  document.getElementById('costo').innerHTML = totale.toFixed(2) + '&euro;';
 });
+
+
 
 
